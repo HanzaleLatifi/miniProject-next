10ac4d183a58fad3f1f4a8ed3512d7de6ccf20f3
h8 <h.latifi03@gmail.com>
2022-09-21 18:47:08 +0430

    develope Header

---
+++ myapp/src/layout/Header.js
@@ -0,0 +1,57 @@
+import React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+
+function Header() {
+  const [display, setDisplay] = useState("column");
+  const [count, setCount] = useState("3");
+
+  const handleDisplay = (event) => {
+    setDisplay(event.target.value);
+  };
+  const handleInput = (event) => {
+    setCount(event.target.value);
+  };
+
+  return (
+    <header>
+      <Box sx={{ margin:'10px'}}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Display</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={display}
+                label="Count of user to show"
+                onChange={handleDisplay}
+              >
+                <MenuItem value={"column"}>Column display</MenuItem>
+                <MenuItem value={"row"}>Row display</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              onChange={handleInput}
+              value={count}
+              id="outlined-basic"
+              label="Number of users to display"
+              variant="outlined"
+            />
+          </Grid>
+        </Grid>
+      </Box>
+    </header>
+  );
+}
+
+export default Header;
